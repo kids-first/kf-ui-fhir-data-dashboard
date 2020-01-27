602e840f1692c9b41d457a589729c8f3bab66fd0
Abby George <abbygeorge@Abbys-MacBook-Pro.local>
2020-01-27 10:49:30 -0500

    :sparkles: Basic results and search UI for FHIR
    App has ability to query both the HAPI and SyntheticMass APIs for results, and do a basic query on the results with an input box.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,160 @@
 import React from 'react';
-import logo from './logo.svg';
+import { fetchResource } from './utils/api';
+import {
+  baseUrl,
+  baseResource,
+  baseResourceDisplayFields,
+  omittedFields,
+} from './config';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      results: {},
+      queryInput: '',
+      resultsFetched: false,
+    };
+  }
+
+  // When page mounts, fetch all Patients
+  async componentDidMount() {
+    let temp = {};
+    this.setState({ resultsFetched: false });
+    temp = await this.fetchResource(`${baseUrl}${baseResource}?_count=100`, temp);
+    this.setState({ results: temp, resultsFetched: true });
+  }
+
+  // Fetches next page if results returned are paginated
+  getNextPage = (data) => {
+    const nextPage = data.link.findIndex((x) => x.relation === 'next');
+    return nextPage > -1 ? data.link[nextPage].url : null;
+  }
+
+  // Used to fetch a resource (ie. Patients, Observations..)
+  fetchResource = async (url, temp) => {
+    const data = await fetchResource(url);
+    const newResources = temp;
+    data.entry.forEach((entry) => {
+      if (entry.fullUrl) {
+        newResources[entry.fullUrl] = entry.resource;
+      }
+    });
+    // Not working for SyntheticMass API (?)
+    // url = this.getNextPage(data);
+    // if (url) {
+    //   return this.fetchResource(url, newResources);
+    // }
+    return newResources;
+  }
+
+  isObject = (obj) => obj === Object(obj);
+
+  renderArrOfObj = (arr) => arr.map((elt, i) => {
+    const str = this.flattenObj(elt, '');
+    return <p key={str.concat(i)}>{ str }</p>;
+  });
+
+  /* eslint-disable no-param-reassign */
+  flattenObj = (obj, str) => {
+    Object.keys(obj).forEach((key) => {
+      if (!omittedFields.includes(key)) {
+        if (this.isObject(obj[key])) {
+          str = str.concat(`${key}: ${this.flattenObj(obj[key], str)}`);
+        } else {
+          str = str.concat(`${key}: ${obj[key]}, `);
+        }
+      }
+    });
+    return str;
+  }
+  /* eslint-enable no-param-reassign */
+
+  updateQuery = (e) => {
+    this.setState({ queryInput: e.target.value });
+  }
+
+  queryResource = async () => {
+    const { queryInput } = this.state;
+    let temp = {};
+    this.setState({ resultsFetched: false });
+    temp = await this.fetchResource(`${baseUrl}${baseResource}?${queryInput}&_count=100`, temp);
+    this.setState({ results: temp, resultsFetched: true });
+  }
+
+  render() {
+    const { results, resultsFetched } = this.state;
+    // Urls in the results list
+    const resultList = Object.keys(results).map((resultKey, i) => {
+      // One result
+      const listItem = results[resultKey];
+      return (
+        <div key={resultKey.concat(i)} className="result">
+          {
+            baseResourceDisplayFields.map((field, j) => {
+              if (listItem[field]) {
+                if (!this.isObject(listItem[field]) && !Array.isArray(listItem[field])) {
+                  return (
+                    <React.Fragment key={field.concat(j)}>
+                      <p className="result-header">{ `${field}: ` }</p>
+                      <p>{ listItem[field].toString() }</p>
+                    </React.Fragment>
+                  );
+                }
+                if (Array.isArray(listItem[field])) {
+                  return (
+                    <div key={field.concat(j)}>
+                      <p className="result-header">{ `${field}:` }</p>
+                      { this.renderArrOfObj(listItem[field]) }
+                    </div>
+                  );
+                }
+                return (
+                  <div key={field.concat(j)}>
+                    <p className="result-header">{ `${field}:` }</p>
+                    { this.flattenObj(listItem[field], '') }
+                  </div>
+                );
+              }
+              return null;
+            })
+          }
+        </div>
+      );
+    });
+
+    return (
+      <div className="app">
+        <h1>Kids First FHIR Cohort Builder</h1>
+        <h2>Query</h2>
+        <div className="app_query">
+          <input
+            className="app_query-input"
+            type="text"
+            onChange={(e) => this.updateQuery(e)}
+          />
+          <button
+            type="button"
+            className="app_query-button"
+            onClick={() => this.queryResource()}
+          >
+            Search
+          </button>
+        </div>
+        <h2>
+          Results (
+          { resultList.length }
+          )
+        </h2>
+        <div className="app_result-list">
+          {
+            resultsFetched ? resultList : <p>Loading...</p>
+          }
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/config.js
@@ -0,0 +1,24 @@
+export const proxyUrl = 'https://damp-castle-44220.herokuapp.com/';
+export const baseUrl = 'https://syntheticmass.mitre.org/v1/fhir/';
+// export const baseUrl = 'http://hapi.fhir.org/baseR4/';
+export const oAuthUrl = 'https://syntheticmass.mitre.org/oauth2/accesstoken';
+export const baseResource = 'Patient';
+export const baseResourceSchemaUrl = (resource) => `http://hl7.org/fhir/${resource}.profile.json`;
+
+export const baseResourceDisplayFields = [
+  'id',
+  'address',
+  'birthDate',
+  'communication',
+  'gender',
+  'maritalStatus',
+  'multipleBirthBoolean',
+  'name',
+  'telecom',
+];
+
+export const omittedFields = [
+  'coding',
+  'extension',
+  'use',
+];
