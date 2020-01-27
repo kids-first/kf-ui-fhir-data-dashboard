a022d8445992fdf559f7242d0168acbbdceb308c
Abby George <abgeorge@umich.edu>
2020-01-27 12:31:22 -0500

    :bug: Initial authorization bug fix
    OAuth request was not returned before the data loaded on the initial page load - fixed the order of resolving.

--- src/config.js
+++ src/config.js
@@ -1,8 +1,17 @@
 export const proxyUrl = 'https://damp-castle-44220.herokuapp.com/';
-// export const baseUrl = 'https://syntheticmass.mitre.org/v1/fhir/';
-export const baseUrl = 'http://hapi.fhir.org/baseR4/';
 export const oAuthUrl = 'https://syntheticmass.mitre.org/oauth2/accesstoken';
 export const baseResource = 'Patient';
+
+const synthea = 'https://syntheticmass.mitre.org/v1/fhir/';
+const hapi = 'http://hapi.fhir.org/baseR4/';
+const getBaseUrl = () => {
+  if (!process.env.REACT_APP_FHIR_API || process.env.REACT_APP_FHIR_API === 'synthea') {
+    return synthea;
+  }
+  return hapi;
+};
+
+export const baseUrl = getBaseUrl();
 
 export const baseResourceDisplayFields = [
   'id',
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,24 +1,23 @@
 import { proxyUrl, oAuthUrl } from '../config';
 
-const fetchToken = async () => new Promise(() => {
-  fetch(`${proxyUrl}${oAuthUrl}`, {
-    method: 'POST',
-    body: JSON.stringify({
-      grantType: 'client_credentials',
-      scopes: 'user/*.read',
-    }),
-    headers: {
-      Accept: 'application/json',
-      Authorization: `Basic ${process.env.REACT_APP_SECRET}`,
-      'Content-Type': 'application/json',
-      Origin: 'https://localhost:3000',
-    },
-  })
-    .then((res) => res.json())
-    .then((data) => {
-      sessionStorage.setItem('accessToken', data.access_token);
-    });
-});
+const fetchToken = async (callback) => fetch(`${proxyUrl}${oAuthUrl}`, {
+  method: 'POST',
+  body: JSON.stringify({
+    grantType: 'client_credentials',
+    scopes: 'user/*.read',
+  }),
+  headers: {
+    Accept: 'application/json',
+    Authorization: `Basic ${process.env.REACT_APP_SECRET}`,
+    'Content-Type': 'application/json',
+    Origin: 'https://localhost:3000',
+  },
+})
+  .then((res) => res.json())
+  .then((data) => {
+    sessionStorage.setItem('accessToken', data.access_token);
+    return callback();
+  });
 
 const fetchWithHeaders = async (url, headers) => fetch(`${proxyUrl}${url}`, {
   headers: {
@@ -26,12 +25,9 @@ const fetchWithHeaders = async (url, headers) => fetch(`${proxyUrl}${url}`, {
     Authorization: `Bearer ${sessionStorage.getItem('accessToken')}`,
   },
 })
-  .then((res) => {
-    if (!res.ok) { throw res; }
-    return res.json();
-  })
+  .then((res) => res.json())
   .then((data) => data)
-  .catch(() => fetchToken().then(fetchWithHeaders(url)));
+  .catch(() => fetchToken(() => fetchWithHeaders(url)));
 
 const fetchResource = async (url) => fetchWithHeaders(url, {
   Accept: 'application/fhir+json;charset=utf-8',
