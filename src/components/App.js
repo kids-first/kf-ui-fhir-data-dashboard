1073400c1bf62defdcc88d7d18c3fe030a605af0
Abby George <abgeorge@umich.edu>
2020-02-06 12:24:05 -0500

    :lipstick: Updating to D3b theme
    Using D3b colors and fonts instead of KF

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import ReduxHomepage from './ReduxHomepage';
 import ReduxResourceDetails from './ReduxResourceDetails';
-import logo from '../img/kf-logo.png';
+import logo from '../img/d3b-cube.svg';
 import './App.css';
 
 class App extends React.Component {
@@ -10,7 +10,7 @@ class App extends React.Component {
     return (
       <Router>
         <div className="app">
-          <img src={logo} alt="Kids First" />
+          <img src={logo} alt="D3b" />
           <h1>FHIR Data Dashboard</h1>
           <Switch>
             <Route path="/:resourceBaseType" component={ReduxResourceDetails} />
