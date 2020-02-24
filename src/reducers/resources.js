ea4b167d4cb32d6ba33290c98b74b6588a36f0e8
Abby George <abgeorge@umich.edu>
2020-02-24 17:09:36 -0500

    :sparkles: Adding back in ability to set flag from command line
    Allowing the user to switch to a different FHIR API from the command line shows the flexibility of the dashbaord.

--- src/reducers/resources.js
+++ src/reducers/resources.js
@@ -2,7 +2,7 @@ import {SET_RESOURCES, SET_API} from '../actions';
 
 const initialState = {
   baseUrl: process.env.REACT_APP_FHIR_API
-    ? `${process.env.REACT_APP_FHIR_API}
+    ? `${process.env.REACT_APP_FHIR_API}`
     : 'http://10.10.1.191:8000/',
   allResources: {},
   allResourcesFetched: false,
