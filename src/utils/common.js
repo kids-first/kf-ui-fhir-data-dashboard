8294b314165e3e49e60c82ce0bdb03ea47428976
Abby George <abgeorge@umich.edu>
2020-03-03 22:14:44 -0500

    :bug: Fixed base resource count bug
    Counting for base resources is by looking at all the resources with profiles and subtracting them from the total.

--- src/utils/common.js
+++ src/utils/common.js
@@ -1,2 +1,23 @@
+import {getResourceCount} from './api';
+
 export const getHumanReadableNumber = value =>
   value.toLocaleString(navigator.language, {minimumFractionDigits: 0});
+
+export const getBaseResourceCount = async (baseUrl, baseType, resources) => {
+  let sum = 0;
+  let total = await getResourceCount(`${baseUrl}${baseType}`);
+  const countedResources = new Set();
+  Object.keys(resources).forEach(key => {
+    if (
+      key &&
+      resources[key] &&
+      resources[key].baseType === baseType &&
+      resources[key].name !== baseType &&
+      !countedResources.has(resources[key].url)
+    ) {
+      countedResources.add(resources[key].url);
+      sum += resources[key].count;
+    }
+  });
+  return total - sum;
+};
