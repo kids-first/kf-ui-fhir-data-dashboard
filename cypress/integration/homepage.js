12528351b900dfb1e15f1233408b0a6120caa09e
Abby George <abgeorge@umich.edu>
2020-05-13 14:46:05 -0500

    :white_check_mark: Adding Cypress and first tests, also adding polyfill for Fetch

---
+++ cypress/integration/homepage.js
@@ -0,0 +1,130 @@
+describe('Homepage', () => {
+  before(() => {
+    cy.server();
+    cy.route({
+      method: 'GET',
+      url: 'https://damp-castle-44220.herokuapp.com/http://hapi.fhir.org/**',
+      response: 'fixture:resources.json',
+    }).as('getResources');
+    cy.visit('/servers');
+    cy.contains('hapi').click();
+    cy.contains('Launch').click();
+    cy.wait('@getResources');
+  });
+
+  beforeEach(() => {
+    cy.get('.header__controls-view')
+      .children('i')
+      .get('.grid')
+      .click();
+    cy.get('.ui.card').should('exist');
+    cy.get('.sortable-table').should('not.exist');
+    cy.get('.prompt').clear();
+  });
+
+  it('loads resources', () => {
+    cy.url().should('include', '/resources');
+    cy.contains('18 total');
+  });
+
+  it('filters search results on input', () => {
+    cy.get('.prompt').type('Pa');
+    cy.get('.ui.search>.results').should($x => {
+      expect($x).to.have.length(1);
+    });
+  });
+
+  it('filters results on search', () => {
+    cy.get('.ui.card').should($x => {
+      expect($x).to.have.length(18);
+    });
+    cy.get('.prompt').type('Patient');
+    cy.get('.ui.search>.results')
+      .should($x => {
+        expect($x).to.have.length(1);
+      })
+      .click();
+    cy.get('.ui.card').should($x => {
+      expect($x).to.have.length(1);
+    });
+  });
+
+  it('selects the correct icons for various resource types', () => {
+    cy.contains('Base type: Patient')
+      .children('div')
+      .should('have.class', 'content')
+      .children('div')
+      .should('have.class', 'description')
+      .children('div')
+      .should('have.id', 'homepage__card-description')
+      .children('i')
+      .should('have.class', 'user');
+    cy.contains('Base type: Specimen')
+      .children('div')
+      .should('have.class', 'content')
+      .children('div')
+      .should('have.class', 'description')
+      .children('div')
+      .should('have.id', 'homepage__card-description')
+      .children('i')
+      .should('have.class', 'lab');
+    cy.contains('Base type: Group')
+      .children('div')
+      .should('have.class', 'content')
+      .children('div')
+      .should('have.class', 'description')
+      .children('div')
+      .should('have.id', 'homepage__card-description')
+      .children('i')
+      .should('have.class', 'users');
+    cy.contains('Base type: Condition')
+      .children('div')
+      .should('have.class', 'content')
+      .children('div')
+      .should('have.class', 'description')
+      .children('div')
+      .should('have.id', 'homepage__card-description')
+      .children('i')
+      .should('have.class', 'bug');
+    cy.contains('Base type: Observation')
+      .children('div')
+      .should('have.class', 'content')
+      .children('div')
+      .should('have.class', 'description')
+      .children('div')
+      .should('have.id', 'homepage__card-description')
+      .children('i')
+      .should('have.class', 'eye');
+    cy.contains('Base type: Practitioner')
+      .children('div')
+      .should('have.class', 'content')
+      .children('div')
+      .should('have.class', 'description')
+      .children('div')
+      .should('have.id', 'homepage__card-description')
+      .children('i')
+      .should('have.class', 'doctor');
+    cy.contains('Base type: Encounter')
+      .children('div')
+      .should('have.class', 'content')
+      .children('div')
+      .should('have.class', 'description')
+      .children('div')
+      .should('have.id', 'homepage__card-description')
+      .children('i')
+      .should('have.class', 'clipboard');
+  });
+
+  it('toggles between grid and list views', () => {
+    cy.get('.ui.card').should('exist');
+    cy.get('.sortable-table').should('not.exist');
+    cy.get('.header__controls-view')
+      .children('i')
+      .get('.list')
+      .click();
+    cy.get('.ui.card').should('not.exist');
+    cy.get('.homepage')
+      .children('div')
+      .should('have.class', 'sortable-table');
+  });
+});
