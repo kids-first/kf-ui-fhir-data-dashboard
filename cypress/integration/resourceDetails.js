01a0f363e3740202c4cbffdc9726656ff6639050
Abby George <abgeorge@umich.edu>
2020-05-18 12:26:39 -0500

    :white_check_mark: adding tests for resource ID details page.

--- cypress/integration/resourceDetails.js
+++ cypress/integration/resourceDetails.js
@@ -13,18 +13,6 @@ describe('Homepage', () => {
         'https://damp-castle-44220.herokuapp.com/http://hapi.fhir.org/baseR4/Patient/123',
       response: 'fixture:resourceDetails.json',
     }).as('getResource');
-    cy.route({
-      method: 'GET',
-      url:
-        'https://damp-castle-44220.herokuapp.com/http://hapi.fhir.org/baseR4/metadata',
-      response: 'fixture:capabilityStatementReferences.json',
-    }).as('getCapabilityStatementReferences');
-    cy.route({
-      method: 'GET',
-      url:
-        'https://damp-castle-44220.herokuapp.com/http://hapi.fhir.org/baseR4/Condition?subject=Patient/123',
-      response: {name: 'Condition'},
-    }).as('getReferences');
     cy.visit('/resources/Patient/id=123');
     cy.wait(['@getStructureDefinition', '@getResource']);
   });
@@ -34,6 +22,48 @@ describe('Homepage', () => {
   });
 
   it('loads resource references', () => {
+    cy.server();
+    cy.route({
+      method: 'GET',
+      url:
+        'https://damp-castle-44220.herokuapp.com/http://hapi.fhir.org/baseR4/metadata',
+      response: 'fixture:capabilityStatementReferences.json',
+    }).as('getCapabilityStatementReferences');
+    cy.route({
+      method: 'GET',
+      url:
+        'https://damp-castle-44220.herokuapp.com/http://hapi.fhir.org/baseR4/Condition?patient=Patient/123',
+      response: 'fixture:referenceDetails.json',
+    }).as('getReferenceDetails');
+    cy.route({
+      method: 'GET',
+      url:
+        'https://damp-castle-44220.herokuapp.com/http://hapi.fhir.org/baseR4/StructureDefinition?url=http://fhir.kids-first.io/StructureDefinition/Condition',
+      response: 'fixture:referenceStructureDefinition.json',
+    }).as('getReferenceSD');
     cy.contains('References').click();
+    cy.wait([
+      '@getCapabilityStatementReferences',
+      '@getReferenceDetails',
+      '@getReferenceSD',
+    ]);
+    cy.contains('Resources that reference')
+      .get('.sortable-table')
+      .eq(0)
+      .children('table')
+      .children('tbody')
+      .children('tr')
+      .should($x => {
+        expect($x).to.have.length(1);
+      });
+    cy.contains('Resources referenced by')
+      .get('.sortable-table')
+      .eq(1)
+      .children('table')
+      .children('tbody')
+      .children('tr')
+      .should($x => {
+        expect($x).to.have.length(0);
+      });
   });
 });
